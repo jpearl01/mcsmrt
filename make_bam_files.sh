ed9993600d915daf77ca4282fa2502d9d6c1c2d6
bhatarchanas <bhatarchanas@yahoo.com>
2018-08-29 10:51:33 -0400

    Removing files from main dir

--- make_bam_files.sh
+++
@@ -1,4 +0,0 @@
-bwa index first_16s_copy.fa
-bwa mem -t 16 first_16s_copy.fa ccs2_First\&Sixth_subset.fa > map_to_first_16s.sam
-sambamba view -S -f bam map_to_first\&sixth_16s.sam -o map_to_first\&sixth_16s.bam
-samtools mpileup -uf first_16s_copy.fa map_to_first\&sixth_16s.bam | bcftools call -mv -o map_to_first\&sixth_16s.vcf
